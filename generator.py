1aa96cea98c975d57ec9ddfb7789ade3893ad90d
Awais-AK <AWAISAKHTAR270@IITKGP.AC.IN>
2022-11-10 21:47:00 +0530

    Add Algorithms and Results

---
+++ generator.py
@@ -0,0 +1,118 @@
+import networkx as nx
+import random
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# generate weigted random connected graph with diff m/n ratios
+# decide number of EVs
+# pick source and destination for each EV, no more than 2 EVs on same source
+# asssign speed, discharge_rate, charging rate
+# find max weight edge on a random path from s->d assign for some k>0 discharge_rate*(1+k)*max_weight_dist/speed as max_battery_capacity
+# assign initial battery
+
+filename = "gen_testcase.txt"
+
+_output_file = open(filename,'w')
+
+min_n, max_n = 6,30
+min_w,max_w = 1,100
+n = random.randint(min_n,max_n)
+min_k,max_k = 3,int(n*0.5)
+min_cr,max_cr = 1,max_w*0.1
+min_dcr,max_dcr = 1,max_w*0.1
+min_sp,max_sp = 1,max_w*0.1
+
+while True:
+    G = nx.erdos_renyi_graph(n, 0.5, seed=random.randint(0,1000), directed=False)
+    # G = nx.karate_club_graph()
+    # G = nx.generators.trees.random_tree(n,seed=random.randint(0,1000))
+    if nx.is_connected(G):
+        break
+
+n = G.number_of_nodes()
+m = G.number_of_edges()
+_output_file.write(str(n)+'\n')
+_output_file.write(str(m)+'\n')
+
+for edg in G.edges(data=True):
+    W = min_w+(max_w-min_w)*random.random()
+    G[edg[0]][edg[1]]['weight'] = W
+    _output_file.write(str(edg[0])+' '+str(edg[1])+' '+str(W)+'\n')    
+
+print("Graph Completed")
+nx.draw(G, with_labels=True)
+plt.show()
+
+k = random.randint(min_k,max_k)
+_output_file.write(str(k)+'\n')    
+
+_output_file.flush()
+
+source_node = list(np.random.choice(list(range(n))+list(range(n)),k,replace=False))
+temp = list(np.random.choice(list(range(n))+list(range(n)),2*n))
+destination_node = []
+id = 0
+i = 0
+while i < k:
+    if temp[id] == source_node[i]:
+        id+=1
+        id = id%(2*n)
+        continue
+    destination_node.append(temp[id])
+    i+=1
+    id+=1
+
+charging_rate = [ min_cr+(max_cr-min_cr)*random.random() for _ in range(k)]
+discharging_rate =[ min_dcr+(max_dcr-min_dcr)*random.random() for _ in range(k)]
+speed = [ min_sp+(max_sp-min_sp)*random.random() for _ in range(k)]
+
+print("Source destination and params completed")
+
+def get_max_weight(path):
+    max_w = -1
+    for i in range(1,len(path)):
+        max_w = max(G[path[i-1]][path[i]]['weight'],max_w)
+    return max_w
+
+iter_count_max_weight = 4
+max_edge_weight = []
+# for i in range(k):
+#     gpath = list(nx.all_simple_paths(G, source=source_node[i], target=destination_node[i]))
+#     ridx = np.random.choice(list(range(len(gpath))),size=min(iter_count_max_weight,len(gpath)),replace=False)
+#     max_w = -1
+#     for j in ridx:
+#         max_w = max(max_w,get_max_weight(gpath[j]))
+#     max_edge_weight.append(max_w)
+
+
+for i in tqdm(range(k)):
+    titer_count_max_weight = iter_count_max_weight
+    gpath = (nx.all_simple_paths(G, source=source_node[i], target=destination_node[i]))
+    lpath = []
+    for pt in gpath:
+        lpath.append(pt)
+        titer_count_max_weight-=1
+        if titer_count_max_weight==0:
+            break
+    max_w = -1
+    for ipath in lpath:
+        max_w = max(max_w,get_max_weight(ipath))
+    max_edge_weight.append(max_w)
+
+print("Max weight Completed")
+
+capacity_to_max_edge_multiplier = 2
+max_battery = [ max_edge_weight[i]*(1+random.random()*capacity_to_max_edge_multiplier)*discharging_rate[i]/speed[i] for i in range(k)]
+initial_battery=[max_battery[i]*random.random() for i in range(k)]
+
+for i in range(k):
+    _output_file.write(str(source_node[i])+' ')
+    _output_file.write(str(destination_node[i])+' ')
+    _output_file.write(str(initial_battery[i])+' ')
+    _output_file.write(str(charging_rate[i])+' ')
+    _output_file.write(str(discharging_rate[i])+' ')
+    _output_file.write(str(max_battery[i])+' ')
+    _output_file.write(str(speed[i])+'\n')
+
+_output_file.close()
---
+++ problem_model/problem.py
@@ -0,0 +1,136 @@
+import numpy as np
+import copy
+import pandas as pd
+import networkx as nx
+
+
+class problem(object):
+    def __init__(self):
+        self.n = 0
+        self.m = 0
+        self.k = 0
+        self.source_node = []
+        self.destination_node = []
+        self.initial_battery = []
+        self.charging_rate = []
+        self.discharging_rate = []
+        self.max_battery = []
+        self.speed = []
+        self.edge_list = dict()
+
+        self._df = pd.DataFrame()
+        self.graph_made = False
+        self.Graphs = []
+        self.current_node = []
+        
+        pass
+
+    def __str__(self):
+        """
+        Needs complete to string funtion
+        """
+        _s = "================================\n"
+        _s += f"Number of cities: {self.n}\n"
+        _s += f"Number of roads: {self.m}\n"
+        _s += f"Number of roads: {self.m}\n================================\n"
+        _s += f"Roads:\n{self.edge_list}\n================================\n"
+        _s += f"{self._df}"
+        return _s
+
+    def time_to_travel(self, i, edg):
+        return (self.edge_list[edg]/self.speed[i])
+
+    def battery_to_travel(self, i, edg):
+        return (self.edge_list[edg]/self.speed[i])*self.discharging_rate[i]
+
+    def battery_usage_on_path(self,i, path):
+        total_battery_usage = 0
+        for j in range(1,len(path)):
+            total_battery_usage+=(self.edge_list[(path[j-1],path[j])]/self.speed[i])*self.discharging_rate[i]
+        return total_battery_usage
+
+    def input(self,filename=None):
+        
+        if type(filename)==type(None):
+            self.n = int(input("Number of cities"))
+            self.m = int(input("Number of roads"))
+            print(f"For next {self.m} lines give input of u,v are city index 0-based w is distance : u v w")
+            for _ in range(self.m):
+                _l = input().split(' ')
+                self.edge_list[(int(_l[0]),int(_l[1]))]=float(_l[2])
+                self.edge_list[(int(_l[1]),int(_l[0]))]=float(_l[2])
+
+            self.k = int(input("Number of Ev's"))
+            print("For each EV input 7 space seperated numbers: Sr Dr Br cr dr Mr sr")
+            for _ in range(self.k):
+                _l = input().split(' ')
+                self.source_node.append(int(_l[0]))
+                self.destination_node.append(int(_l[1]))
+                self.initial_battery.append(float(_l[2]))
+                self.charging_rate.append(float(_l[3]))
+                self.discharging_rate.append(float(_l[4]))
+                self.max_battery.append(float(_l[5]))
+                self.speed.append(float(_l[6]))
+        else:
+            _input_file = open(filename,'r')
+
+            self.n = int(_input_file.readline())
+            self.m = int(_input_file.readline())
+            for _ in range(self.m):
+                _l = _input_file.readline().split(' ')
+                self.edge_list[(int(_l[0]),int(_l[1]))]=float(_l[2])
+                self.edge_list[(int(_l[1]),int(_l[0]))]=float(_l[2])
+
+            self.k = int(_input_file.readline())
+            for _ in range(self.k):
+                _l = _input_file.readline().split(' ')
+                self.source_node.append(int(_l[0]))
+                self.destination_node.append(int(_l[1]))
+                self.initial_battery.append(float(_l[2]))
+                self.charging_rate.append(float(_l[3]))
+                self.discharging_rate.append(float(_l[4]))
+                self.max_battery.append(float(_l[5]))
+                self.speed.append(float(_l[6]))
+
+            _input_file.close()
+        
+        self._df["Sr"] = self.source_node
+        self._df["Dr"] = self.destination_node
+        self._df["Br"] = self.initial_battery
+        self._df["cr"] = self.charging_rate
+        self._df["dr"] = self.discharging_rate
+        self._df["Mr"] = self.max_battery
+        self._df["sr"] = self.speed
+
+        self.current_node = copy.deepcopy(self.source_node)
+
+    def make_graphs(self):
+        if self.graph_made:
+            return
+        for i in range(self.k):
+            G = nx.Graph()
+            for edg in self.edge_list:
+                if self.battery_to_travel(i,edg) > self.max_battery[i]:
+                    continue
+                G.add_edge(edg[0],edg[1],weight=self.time_to_travel(i,edg))
+            self.Graphs.append(G)
+        self.graph_made = True
+        return
+
+    def theoritical_minima(self):
+        """
+        returns theoritical minimum time for all EVs to reach the destination
+        """
+        traveling_time = []
+        charging_time = []
+        total_time = []
+        for i in range(self.k):
+            spath = nx.shortest_path(self.Graphs[i],source=self.source_node[i],target=self.destination_node[i], weight='weight')
+            # print(spath)
+            b = self.battery_usage_on_path(i,spath)
+            traveling_time.append(b/self.discharging_rate[i])
+            charging_time.append(max(b-self.initial_battery[i],0)/self.charging_rate[i])
+            total_time.append(traveling_time[-1]+charging_time[-1])
+        # print(total_time)
+        return np.max(np.array(total_time))
+
